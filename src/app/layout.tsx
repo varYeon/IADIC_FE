b910167b3c8f4e66ce960ccdf516eec91f83cce8
varYeon <xjkooy100@gmail.com>
2026-07-08 17:55:08 +0900

    chore : tanstack query 설치 및 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import ToastProvider from "@/components/common/toast/ToastProvider";
 import Header from "@/components/layout/Header";
 import Nav from "@/components/layout/Nav";
+import QueryProvider from "@/components/QueryProvider";
 import "../css/index.css";
 
 const pretendard = localFont({
@@ -26,18 +27,20 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${pretendard.variable}`} suppressHydrationWarning>
       <body className={`${pretendard.className} bg-[#E5E8EB] px-0 antialiased dark:bg-[#272727]`}>
-        <ThemeProvider attribute="class">
-          <div className="bg-bg-main m-auto flex min-h-dvh w-full max-w-full flex-col rounded-none 2xl:max-w-[1536px] 2xl:rounded-[30px]">
-            <Header />
-            <div className="flex h-fit flex-1 flex-col sm:flex-row">
-              <Nav />
-              <main className="scrollbar-hide flex h-[calc(100vh-var(--header-height))] flex-1 rounded-br-[30px] sm:overflow-y-scroll">
-                {children}
-                <ToastProvider />
-              </main>
+        <QueryProvider>
+          <ThemeProvider attribute="class">
+            <div className="bg-bg-main m-auto flex min-h-dvh w-full max-w-full flex-col rounded-none 2xl:max-w-[1536px] 2xl:rounded-[30px]">
+              <Header />
+              <div className="flex h-fit flex-1 flex-col sm:flex-row">
+                <Nav />
+                <main className="scrollbar-hide flex h-[calc(100vh-var(--header-height))] flex-1 rounded-br-[30px] sm:overflow-y-scroll">
+                  {children}
+                  <ToastProvider />
+                </main>
+              </div>
             </div>
-          </div>
-        </ThemeProvider>
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
